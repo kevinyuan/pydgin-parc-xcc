4d044714afd1718b69f6ed5b667d544a9b35c5b1
Rimas Avizienis <rimas@eecs.berkeley.edu>
2009-09-09 18:46:26 -0700

    Added lock and recursive lock implementations to newlib (used by malloc)

--- src/newlib/libc/stdlib/mlock.c
+++ src/newlib/libc/stdlib/mlock.c
@@ -1,3 +1,83 @@
+#include <sys/lock.h>
+
+#pragma weak hart_self
+int hart_self()
+{
+  int procid;
+  __asm__ __volatile__ ("mfc0 %0, $15, 1" : "=r"(procid));
+  procid = procid & 0x000003FF;
+  return procid;
+}
+
+#pragma weak newlib_trylock
+int newlib_trylock(int* lock)
+{
+  int reg = 1;
+  __asm__ __volatile__ ("amo.or %0, %1, %2" : "=r"(reg) : "r"(lock), "0"(reg) : "memory");
+//  __asm__ __volatile__ ("amo.swap %0, 0(%2)" : "=r"(reg) : "r"(reg), "r"(lock) : "memory");
+//  __asm__ __volatile__ ("swap [%2],%0" : "=r"(reg) : "0"(reg),"r"(lock) : "memory");
+  return reg;
+}
+
+#pragma weak newlib_lock
+void newlib_lock(int* lock)
+{
+  while(newlib_trylock(lock))
+    while(*(volatile int*)lock);
+}
+
+#pragma weak newlib_is_locked
+int newlib_is_locked(int* lock)
+{
+  return *(volatile int*)lock;
+}
+
+#pragma weak newlib_recursive_trylock
+int newlib_recursive_trylock(long long* lock)
+{
+  unsigned int lower = *(volatile unsigned int*)lock;
+  unsigned int id = hart_self();
+  if(newlib_is_locked((int*)lock+1) && id+1 == (lower >> 8))
+  {
+    if((lower & 0xFF) == 0xFF)
+        __asm__ __volatile__ ("li $v0, %0; syscall" : : "i"(65535) : "v0"); //cause a panic
+//      __asm__ __volatile__("ta 0x7f"); /* too many recursive acquisitions */
+    *(volatile unsigned int*)lock = lower+1;
+    return 0;
+  }
+  else if(newlib_trylock((int*)lock+1) == 0)
+  {
+    *(volatile unsigned int*)lock = ((id+1)<<8)+1;
+    return 0;
+  }
+  else return -1;
+}
+
+#pragma weak newlib_recursive_lock
+void newlib_recursive_lock(long long* lock)
+{
+  if(newlib_recursive_trylock(lock) != 0)
+  {
+    newlib_lock((int*)lock+1);
+    unsigned int id = hart_self();
+    *(volatile unsigned int*)lock = ((id+1)<<8)+1;
+  }
+}
+
+#pragma weak newlib_recursive_unlock
+void newlib_recursive_unlock(long long* lock)
+{
+  int id = hart_self();
+  unsigned int lower = *(volatile unsigned int*)lock;
+  if(!(newlib_is_locked((int*)lock+1) && id+1 == (lower >> 8)))
+    __asm__ __volatile__ ("li $v0, %0; syscall" : : "i"(65535) : "v0"); //cause a panic
+//  __asm__ __volatile__("ta 0x7f"); /* oops, someone released a free or unowned lock */
+  else if((lower & 0xFF) == 1)
+    *(volatile long long*)lock = 0;
+  else
+    *(volatile unsigned int*)lock = lower-1;
+}
+
 #ifndef MALLOC_PROVIDED
 /*
 FUNCTION
