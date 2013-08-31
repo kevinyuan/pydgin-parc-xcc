8ff475696af9d7cdffa05ce2e8fb4ad59c9e103d
Shreesha Srinath <shreesha@csl.cornell.edu>
2013-08-31 13:14:14 -0400

    [macro] added macro instruciton to gas

--- src/opcodes/mips-maven-opc.c
+++ src/opcodes/mips-maven-opc.c
@@ -278,6 +278,9 @@ const struct mips_opcode mips_builtin_opcodes[] =
 // shreesha - for.r instruction
 {"for.r",          "s,t,p",     0xc8000000, 0xfc000000, CBD|RD_s|RD_t,                0,              INSN_MAVEN        },
 
+// shreesha - macro instruction
+{"macro",          "s,p",       0xd4000000, 0xfc000000, CBD|RD_s,                     0,              INSN_MAVEN        },
+
 /* Maven Vector-Thread Instructions - Vector Memory Instructions */
 
 //NOTE: these dependencies are not used and may be incorrect if optimizations are turned on!
