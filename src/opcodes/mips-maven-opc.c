75c3473ba353267e795d320a4f3c6986aa5bc832
Shreesha Srinath <shreesha@csl.cornell.edu>
2014-05-28 14:08:16 -0400

    [rpdn] added support for hint.wl instruction

--- src/opcodes/mips-maven-opc.c
+++ src/opcodes/mips-maven-opc.c
@@ -296,6 +296,9 @@ const struct mips_opcode mips_builtin_opcodes[] =
 // shreesha - subu.xi instruction
 {"subu.xi",        "d,v,t",     0x9c000011, 0xfc0007ff, SY|SM|WR_d|RD_s|RD_t,         0,              INSN_MAVEN        },
 
+// shreesha - hint.wl instruction
+{"hint.wl",        "d,v,t",     0x9c000012, 0xfc0007ff, SY|SM|WR_d|RD_s|RD_t,         0,              INSN_MAVEN        },
+
 /* Maven Vector-Thread Instructions - Vector Memory Instructions */
 
 //NOTE: these dependencies are not used and may be incorrect if optimizations are turned on!
