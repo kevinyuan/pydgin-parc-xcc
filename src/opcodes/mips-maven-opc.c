3ef05072d5af91722fdaff146f8e73785ed7454e
Shreesha Srinath <shreesha@csl.cornell.edu>
2015-03-07 13:44:10 -0500

    [mtxcel,mfxcel] added support for mtxcel and mfxcel instructions

--- src/opcodes/mips-maven-opc.c
+++ src/opcodes/mips-maven-opc.c
@@ -323,6 +323,10 @@ const struct mips_opcode mips_builtin_opcodes[] =
 // shreesha - xret instruction -- rs, rt, rd should always be zero
 {"xret",           "s",         0x00000028, 0xfc1fffff, UBD|RD_s,                     0,              INSN_MAVEN        },
 
+// shreesha - mtxcel,mfxcel instructions
+{"mtxcel",         "s,t,i",     0x48000000, 0xfc00f800, RD_s|RD_t,                    0,              INSN_MAVEN        },
+{"mfxcel",         "s,t,i",     0x48000800, 0xfc00f800, RD_s|RD_t,                    0,              INSN_MAVEN        },
+
 /* Maven Vector-Thread Instructions - Vector Memory Instructions */
 
 //NOTE: these dependencies are not used and may be incorrect if optimizations are turned on!
